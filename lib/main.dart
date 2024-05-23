37a45128161f74cb4d0c6c02f96b076e39db72ed
saravanacollabrain <123923487+saravana758@users.noreply.github.com>
2024-05-23 16:25:28 +0530

    k

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,8 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-     // home: const MyHomePage(),
-     home: const DashboardScreen(),
+      home: const MyHomePage(),
+    
     );
   }
 }
